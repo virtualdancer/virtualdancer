054f91e2769a55dc84488ffe6db680dce94bd1c5
Maël <144245854+virtualdancer@users.noreply.github.com>
2023-12-20 15:06:42 +0000

    maj

--- script.js
+++ script.js
@@ -1,55 +1,33 @@
-var menu = document.querySelector('.nav__list');
-var burger = document.querySelector('.burger');
-var doc = $(document);
-var l = $('.scrolly');
-var panel = $('.panel');
-var vh = $(window).height();
-
-var openMenu = function() {
-  burger.classList.toggle('burger--active');
-  menu.classList.toggle('nav__list--active');
-};
-
-// reveal content of first panel by default
-panel.eq(0).find('.panel__content').addClass('panel__content--active');
-
-var scrollFx = function() {
-  var ds = doc.scrollTop();
-  var of = vh / 4;
-  
-  // if the panel is in the viewport, reveal the content, if not, hide it.
-  for (var i = 0; i < panel.length; i++) {
-    if (panel.eq(i).offset().top < ds+of) {
-     panel
-       .eq(i)
-       .find('.panel__content')
-       .addClass('panel__content--active');
-    } else {
-      panel
-        .eq(i)
-        .find('.panel__content')
-        .removeClass('panel__content--active')
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+    
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+    
+            everyitem.addEventListener('mouseover', function(e){
+    
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+    
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+    
+    
+            })
+        });
+    
     }
-  }
-};
-
-var scrolly = function(e) {
-  e.preventDefault();
-  var target = this.hash;
-  var $target = $(target);
-
-  $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-  }, 300, 'swing', function () {
-      window.location.hash = target;
-  });
-}
-
-var init = function() {
-  burger.addEventListener('click', openMenu, false);
-  window.addEventListener('scroll', scrollFx, false);
-  window.addEventListener('load', scrollFx, false);
-  $('a[href^="#"]').on('click',scrolly);
-};
-
-doc.on('ready', init);
+    // end if innerWidth
+    });
